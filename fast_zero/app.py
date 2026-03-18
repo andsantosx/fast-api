18ea33662285f6155bb242b0074333cf08ae7ef4
andsantosx <ardrisonsantos321@gmail.com>
2026-03-18 18:45:30 -0300

    feat: first schema

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from http import HTTPStatus
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(title='API de Estudos', description='API para estudos de FastAPI', version='0.1.0')
 
 
-@app.get('/')
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_model=Message
+)
 def read_root():
     return {'message': 'Olá Mundo!'}
